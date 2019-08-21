12094d14b4b58f188dd2b2a4b8b5b88bc7fce1c8
endo2 <kaji17@outlook.jp>
2019-08-21 00:30:21 +0000

    5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,33 @@
 class PostsController < ApplicationController
+  def new
+  end
+ def create
+  @post = Post.new(content: params[:content])
+  @post.save
+  redirect_to posts_index_url
+ end  
+  
    def index
-    @posts  =   Post.all
+    @posts  =   Post.all.order(created_at: :desc)
    end
-end   
+  
+   def show
+    @post = Post.find(params[:id])
+   end 
+    
+   def edit
+    @post = Post.find(params[:id])
+   end
+   def update
+    @post = Post.find(params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to posts_index_url
+   end
+   
+   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_index_url
+   end 
+end    
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   root 'home#top'
-  get 'top',to: 'home#top'
+  get 'top', to: 'home#top'
   get 'about', to: 'home#about'
-  get 'posts/index',to:'posts#index'
+  get 'posts/new', to: 'posts#new'
+  post 'posts/create', to: 'posts#create'
+  get 'posts/index', to: 'posts#index'
+  get 'posts/:id', to: 'posts#show', as: :post
+  get 'posts/:id/edit', to: 'posts#edit', as: :edit_post
+  post 'posts/:id/update', to: 'posts#update', as: :update_post
+  post 'posts/:id/destroy', to: 'posts#destroy', as: :destroy_post
+
+ 
 end
+  
