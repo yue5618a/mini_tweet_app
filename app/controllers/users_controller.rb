4ccc8c91d29b89be34ad7eb1e2c4cfb0005f78e9
endo2 <kaji17@outlook.jp>
2019-08-25 00:36:39 +0000

    6.9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
+
 class UsersController < ApplicationController
   def index
-     @users = User.all
+    @users = User.all
   end
+
   def show
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+   if @user.save
+    flash[:notice] = 'ユーザーを新規登録しました！'
+    redirect_to user_url @user
+   else
+    render :new 
+   end
   end
 end
