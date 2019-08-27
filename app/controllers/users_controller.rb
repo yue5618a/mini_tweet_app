2077fb5b5e72f5d4aed67439033a451e02f19bca
endo2 <kaji17@outlook.jp>
2019-08-27 14:01:04 +0000

    8.6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,17 @@ class UsersController < ApplicationController
   def login_page
   end
  
-  def login
-  end  
+ def login
+   @user = User.find_by(email: params[:email], password: params[:password])
+  if @user
+    flash[:notice] = "ログインしました。"
+    redirect_to posts_index_url
+  else
+    @error_message = "メールアドレス又はパスワードが間違っています。"
+    @email = params[:email]
+    @password = params[:password]
+    render :login_page
+  end
+ end
   
 end 
